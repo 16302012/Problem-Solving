60da7bab082349caabb789419d73bf9427b93481
Raisa Azad <raisaazad55@gmail.com>
2022-05-12 22:17:22 +0600

    Looping through gen

--- time difference counting.py
+++ time difference counting.py
@@ -15,6 +15,13 @@ def performance(func):
     return wrapper
 @performance 
 def long_time():
+    print('1')
     for i in range(100000000):
         i*5
-long_time() 
+@performance
+def long_time2():
+    print('2')
+    for i in range(100000000):
+        i*5
+long_time()
+long_time2() 
