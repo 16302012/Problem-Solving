7bb50ac834c0c540f325aa4fdd7bdf186e16200a
Raisa Azad <raisaazad55@gmail.com>
2022-05-13 15:15:16 +0600

    division

---
+++ division.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 13 15:10:47 2022
+
+@author: User
+"""
+
+#list=[]
+for i in range(2000,3200):
+    if i%7==0 and i%5!=0:
+        print(i,end=',')
+print("\b")
