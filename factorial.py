d75ad1939d78ea20e70095064052caf9b628424c
Raisa Azad <raisaazad55@gmail.com>
2022-05-13 15:30:22 +0600

    factorial

---
+++ factorial.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 13 15:18:44 2022
+
+@author: User
+"""
+
+num = int(input())
+fact = 1
+i = 1
+while i<=num:
+    fact = fact*i
+    i=i+1
+print(fact)
