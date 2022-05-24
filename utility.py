2d383bf7980775263b4abb7a9a01562715d5b5cc
Raisa Azad <raisaazad55@gmail.com>
2022-05-24 23:41:03 +0600

    utility.py

---
+++ utility.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 24 23:34:22 2022
+
+@author: User
+"""
+
+def multiply(num1, num2):
+    return num1*num2
+def divide(num1, num2):
+    return num1/num2
