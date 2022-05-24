cd0349a97bd6c04543f4c9b6bbada0760af8da3d
Raisa Azad <raisaazad55@gmail.com>
2022-05-25 00:12:51 +0600

    main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 24 23:42:28 2022
+
+@author: User
+"""
+
+
+import utility
+import shopping.shopping_cart
+
+#print(utility.divide(15,3))
+print(shopping.shopping_cart.buy('orange','banana'))
