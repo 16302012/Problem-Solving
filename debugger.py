6230d855190938da1894efd7f09a6decfa4de838
Raisa Azad <raisaazad55@gmail.com>
2022-05-26 15:25:15 +0600

    debugger.py

---
+++ debugger.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 26 15:15:06 2022
+
+@author: User
+"""
+
+import pdb
+
+def add(num1, num2):
+    pdb.set_trace()
+    return num1+num2
+
+add(4, 'hi')
