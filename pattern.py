7991a68cfafed4dd0db8dcd42ab63832041511b4
Raisa Azad <raisaazad55@gmail.com>
2022-05-31 21:14:58 +0600

    pattern.py

---
+++ pattern.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 31 21:08:38 2022
+
+@author: User
+"""
+
+for num in range(10):
+    for i in range(num):
+        print (num, end=" ") #print number
+    # new line after each row to display pattern correctly
+    print("\n")
