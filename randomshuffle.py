8db5f91dc5bf6bf46cd9388af003131b7a47fb35
Raisa Azad <raisaazad55@gmail.com>
2022-06-05 11:07:08 +0600

    randomshuffle.py

---
+++ randomshuffle.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  5 11:01:02 2022
+
+@author: User
+"""
+
+import random
+char_list = ['a','e','i','o','u']
+random.shuffle(char_list)
+print(''.join(char_list))
