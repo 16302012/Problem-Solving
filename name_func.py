62f2316571d6f424d508c91f351187500365fb46
Raisa Azad <raisaazad55@gmail.com>
2022-06-08 17:49:00 +0600

    name_func.py

---
+++ name_func.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  8 17:48:01 2022
+
+@author: User
+"""
+
+def get_formatted_name(first, last):
+    full_name = first + ' ' + last
+    return full_name.title()
