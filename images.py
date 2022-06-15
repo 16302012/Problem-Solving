760d9c7159b03de8b424bd879b47b3eae28a472d
Raisa Azad <raisaazad55@gmail.com>
2022-06-15 19:54:14 +0600

    JPGtoPNGconvert.py

--- images.py
+++ images.py
@@ -7,19 +7,25 @@ Created on Thu Jun  9 20:09:22 2022
 
 from PIL import Image, ImageFilter
 
-img = Image.open('./pokedex/pikachu.jpg.jpg')
-filtered_img = img.convert('L')
+img = Image.open('./astro.jpg.jpg')
+#filtered_img = img.convert('L')
 #print(img.format)
 #print(img.size)
 #print(img.mode)
 #print(dir(img))
 #filtered_img.save("blur.png",'png') 
 #PNG supports image filters which JPEG don't
-resize = filtered_img.resize((300,300))
-resize.save("grey.png",'png')  
+#resize = filtered_img.resize((300,300))
+#resize.save("grey.png",'png')  
 #filtered_img.show()
 
 #CropImages
-box=(100,100,400,400)
-region = filtered_img.crop(box)
-region.save("grey.png",'png')  
+#box=(100,100,400,400)
+#region = filtered_img.crop(box)
+#region.save("grey.png",'png')  
+
+
+#astro.jpg
+print(img.size)
+new_img = img.resize((400,400))
+new_img.save('thumbnail.jpg')
